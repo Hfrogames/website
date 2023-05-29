1f383a692ee13f510c2aca1638a56755a7510188
ayoayomide <ayoayomide124@gmail.com>
2023-05-29 14:59:56 +0100

    home page fixed

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+ <div>
+   <h1 class="logo-txt">HFROGAMES</h1>
+   <p>fun for everone</p>
+   <h1>Coming Soon</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
